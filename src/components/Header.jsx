17faa332ec2b6c25ef19c2bf0611526ae1daf4ab
ivanmunoz99 <ivandariomunos99@gmail.com>
2022-12-22 18:04:49 -0500

    navegation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
 import React from "react"
 import styleHeader from "./Header.module.css"
+import { useNavigate } from 'react-router-dom';
 
 const Header = () =>{
-    
+    const history = useNavigate();
+    const handleClick = () => history('/cart');
+    const first = useNavigate();
+    const Click = () => first('/products');
     return (
         <div className={styleHeader.header}>
-            <img className={styleHeader.logo} src="https://firebasestorage.googleapis.com/v0/b/distribuidora-84a8f.appspot.com/o/logo.png?alt=media&token=4e40843f-5ca5-409b-9895-1f1d995ba64b" alt="Logo distribuidora el rolo" />
-            <img className={styleHeader.cart} src="https://firebasestorage.googleapis.com/v0/b/distribuidora-84a8f.appspot.com/o/grocery-store.png?alt=media&token=6ef700c8-3ea4-453b-b610-9ac490095565" alt="Icono carrito de compras" />
+            <img onClick={Click} className={styleHeader.logo} src="https://firebasestorage.googleapis.com/v0/b/distribuidora-84a8f.appspot.com/o/logo.png?alt=media&token=4e40843f-5ca5-409b-9895-1f1d995ba64b" alt="Logo distribuidora el rolo" />
+            <img onClick={handleClick} className={styleHeader.cart} src="https://firebasestorage.googleapis.com/v0/b/distribuidora-84a8f.appspot.com/o/grocery-store.png?alt=media&token=6ef700c8-3ea4-453b-b610-9ac490095565" alt="Icono carrito de compras" />
         </div>
     )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react"
-
 import styleHome from "./Home.module.css"
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
 
-    
+    const history = useNavigate();
+    const handleClick = () => history('/products');
 
     return(
         <>
@@ -15,7 +16,7 @@ const Home = () => {
             height={200}
             src="https://firebasestorage.googleapis.com/v0/b/distribuidora-84a8f.appspot.com/o/logo.png?alt=media&token=4e40843f-5ca5-409b-9895-1f1d995ba64b" alt="logo" />
             <p className={styleHome.text}>Me alegra que estés aquí.<span className={styleHome.textColor}> Distribuidora el rolo</span>  tiene la mejor variedad de licores a domicilio, podrás elegir y sumar la cantidad de productos que quieras.</p>
-            <button>COMPRAR!</button>
+            <button onClick={handleClick}>COMPRAR!</button>
         </div>
         <div className={styleHome.warning}>
             <img className={styleHome.img} src="https://firebasestorage.googleapis.com/v0/b/distribuidora-84a8f.appspot.com/o/Instagram_logo.svg.png?alt=media&token=beca55ad-87f8-45f5-9083-f6a524a321d0" alt="red social" />
