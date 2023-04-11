26b9df47b7ab8e5e8d3d1f3caa6206e8496ea585
ivanmunoz99 <ivandariomunos99@gmail.com>
2023-04-11 14:13:36 -0500

    pull dev

--- src/pages/finish.js
+++ src/pages/finish.js
@@ -11,7 +11,7 @@ const Finish = () => {
     //console.log(addList.map(item => `${item.title} ${item.price} ${item.volume + "ml"} ${item.cuantity}`))
     const description = addList.map(item => `${'Producto: ' + item.section}  ${item.title} ${item.volume + "ml"} ${'Cantidad: ' + item.cuantity}`)
     console.log(description);
-    
+
     // reduce() is returning a single value as a result
     const subTotal = addList.reduce((total, item) => {
         if (item.cuantity > 1) return total + item.price * item.cuantity
@@ -19,28 +19,28 @@ const Finish = () => {
     }, 0)
     return (
         <>
-            <Header/>
-        <div className={styleCart.finish}>
-            <div className={styleCart.total}><h2>TOTAL</h2></div>
-            <div className={styleCart.details}>
-                <p><strong>Subtotal:</strong></p>
-                <p>$ {subTotal}</p>
+            <Header />
+            <div className={styleCart.finish}>
+                <div className={styleCart.total}><h2>TOTAL</h2></div>
+                <div className={styleCart.details}>
+                    <p><strong>Subtotal:</strong></p>
+                    <p>$ {subTotal}</p>
+                </div>
+                <hr />
+                <div className={styleCart.details}>
+                    <p><strong>Envio:</strong></p>
+                    <p>$ 6000</p>
+                </div>
+                <hr />
+                <div className={styleCart.details}>
+                    <p><strong>Total:</strong></p>
+                    <p><strong>$ {subTotal + 6000}</strong></p>
+                </div>
+                <button onClick={() => window.open(`https://api.whatsapp.com/send/?phone=573057150737&text= ¡hola! para un domicilio del siguiente ${description} ${'Valor total:'} ${subTotal + 6000}$`, "_blank")}>Finalizar Compra</button>
+                <img src="https://firebasestorage.googleapis.com/v0/b/distribuidora-84a8f.appspot.com/o/logo.png?alt=media&token=4e40843f-5ca5-409b-9895-1f1d995ba64b" alt="Logo distribuidora el rolo"></img>
             </div>
-            <hr />
-            <div className={styleCart.details}>
-                <p><strong>Envio:</strong></p>
-                <p>$ 6000</p>
-            </div>
-            <hr />
-            <div className={styleCart.details}>
-                <p><strong>Total:</strong></p>
-                <p><strong>$ {subTotal + 6000}</strong></p>
-            </div>
-            <button onClick={() => window.open(`https://api.whatsapp.com/send/?phone=573057150737&text= ¡hola! para un domicilio del siguiente ${description} ${'Valor total:'} ${subTotal + 6000}$`, "_blank")}>Finalizar Compra</button>
-            <img src="https://firebasestorage.googleapis.com/v0/b/distribuidora-84a8f.appspot.com/o/logo.png?alt=media&token=4e40843f-5ca5-409b-9895-1f1d995ba64b" alt="Logo distribuidora el rolo"></img>
-        </div>
         </>
-        
+
     )
 }
 
